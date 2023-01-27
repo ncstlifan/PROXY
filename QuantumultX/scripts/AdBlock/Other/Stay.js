5dcf5daf53fc096e0e36f7f8ce83846fdf2e6a86
李凡 <84082145@qq.com>
2023-01-27 16:17:10 +0800

    删库

--- QuantumultX/scripts/AdBlock/Other/Stay.js
+++
@@ -1,7 +0,0 @@
-
-
-let response = JSON.parse($response.body);
-if (response.biz) {
-    response.biz = Object.values(response.biz).filter(item => !(item["type"]=="promoted"));
-}
-$done({ body: JSON.stringify(response) });
